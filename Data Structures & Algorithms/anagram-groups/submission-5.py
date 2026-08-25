0426341780c48c7b4e884d6dd3e049637be7f409
Tarun Patanjali <78391289+tarpat@users.noreply.github.com>
2026-08-24 22:59:17 -0500

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        char_counts = dict()
+        for string in strs:
+            freq = [0] * 26
+            for c in string.lower():
+                freq[ord(c) - 97] += 1
+
+            if tuple(freq) in char_counts:
+                char_counts[tuple(freq)].append(string)
+            else:
+                char_counts[tuple(freq)] = [string]
+        
+        return list(char_counts.values())
